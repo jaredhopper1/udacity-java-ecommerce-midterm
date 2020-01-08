6b4f0abd2390496405f6f4fd2acf4a3d24cb42a6
jahopper <jahopper@deloitte.com>
2020-01-08 15:54:53 -0600

    PostReview and GetReview

---
+++ src/test/java/com/udacity/course3/reviews/repos/ReviewDocumentRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.udacity.course3.reviews.repos;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.udacity.course3.reviews.entities.Comment;
+import com.udacity.course3.reviews.entities.Product;
+import com.udacity.course3.reviews.entities.Review;
+import com.udacity.course3.reviews.entities.ReviewDocument;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+
+public class ReviewDocumentRepositoryTest {
+    @Autowired
+    private ReviewDocumentRepository reviewDocumentRepository;
+
+   @AfterEach
+    public void tearDown(){reviewDocumentRepository.deleteAll();}
+
+    @Test
+    public void testPostReview() {
+
+        // insert a new review document
+        ReviewDocument review = new ReviewDocument();
+        review.setId(1);
+        review.setReview("This is confusing");
+
+
+        // ensure that the returned object that was save is not null
+        Assert.assertNotNull(reviewDocumentRepository.save(review));
+
+    }
+
+    @Test
+    public void TestGetReview() {
+      //new review document
+       Product product = new Product();
+       product.setId(1);
+       product.setName("Forky");
+       product.setDescription("It is a fork");
+       product.setCategory("Action figure");
+
+       ReviewDocument review = new ReviewDocument();
+       review.setId(1);
+       review.setTitle("test");
+       review.setReview("Testing review");
+       review.setProduct(product);
+
+       Assert.assertNotNull(reviewDocumentRepository.save(review));
+
+
+       //get document from db and test values that were retrieved
+        Assert.assertNotNull(reviewDocumentRepository.findAllByProductId(1));
+        List<ReviewDocument> returnedReviewSize =
+                reviewDocumentRepository.findAllByProductId(1);
+        Assert.assertEquals(1, returnedReviewSize.size());
+        Assert.assertNotNull(reviewDocumentRepository.findAllByProductId(review.getId()));
+
+        ReviewDocument returnedReviewsContent =
+                reviewDocumentRepository.findById(1).get();
+        assertEquals(1, returnedReviewsContent.getId().intValue());
+        assertEquals("test", returnedReviewsContent.getTitle());
+        assertEquals("Testing review", returnedReviewsContent.getReview());
+
+
+
+
+
+    }
+}
