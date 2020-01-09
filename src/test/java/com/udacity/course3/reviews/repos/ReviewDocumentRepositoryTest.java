ce774dd91a2c34d4769cfddea8b1fc01a7b16971
jahopper <jahopper@deloitte.com>
2020-01-09 14:08:21 -0600

    all tests working

--- src/test/java/com/udacity/course3/reviews/repos/ReviewDocumentRepositoryTest.java
+++ src/test/java/com/udacity/course3/reviews/repos/ReviewDocumentRepositoryTest.java
@@ -8,12 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.udacity.course3.reviews.entities.Comment;
 import com.udacity.course3.reviews.entities.Product;
-import com.udacity.course3.reviews.entities.Review;
 import com.udacity.course3.reviews.entities.ReviewDocument;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -27,33 +25,7 @@ public class ReviewDocumentRepositoryTest {
     @Autowired
     private ReviewDocumentRepository reviewDocumentRepository;
 
-    Product product = null;
-    ReviewDocument review = null;
-    Comment comment = null;
 
-    @BeforeEach
-    private void setup(){
-        Product product = new Product();
-        product.setId(1);
-        product.setName("Forky");
-        product.setDescription("It is a fork");
-        product.setCategory("Action figure");
-
-        ReviewDocument review = new ReviewDocument();
-        review.setId(1);
-        review.setTitle("test");
-        review.setReview("Testing review");
-        review.setProduct(product);
-
-        Comment comment = new Comment();
-        comment.setId(1);
-        comment.setHeading("Test Comment");
-        comment.setComment("This is a comment body");
-
-        List<Comment> commentList = new ArrayList<Comment>();
-        commentList.add(comment);
-        review.setComments(commentList);
-    }
 
    @AfterEach
     public void tearDown(){reviewDocumentRepository.deleteAll();}
@@ -75,12 +47,28 @@ public class ReviewDocumentRepositoryTest {
     @Test
     public void TestGetReview() {
 
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Forky");
+        product.setDescription("It is a fork");
+        product.setCategory("Action figure");
 
-        Product productCheck = new Product();
-        ReviewDocument reviewCheck = new ReviewDocument();
+        ReviewDocument review = new ReviewDocument();
+        review.setId(1);
+        review.setTitle("test");
+        review.setReview("Testing review");
+        review.setProduct(product);
 
-        productCheck = product;
-        reviewCheck = review;
+        Comment comment = new Comment();
+        comment.setId(1);
+        comment.setHeading("Test Comment");
+        comment.setComment("This is a comment body");
+
+        List<Comment> commentList = new ArrayList<Comment>();
+        commentList.add(comment);
+        review.setComments(commentList);
+
+        reviewDocumentRepository.save(review);
 
 
        //check to see if review was saved to db
@@ -104,63 +92,64 @@ public class ReviewDocumentRepositoryTest {
 
     @Test
     public void testPostComments() {
-        Product productCheck = new Product();
-        ReviewDocument reviewCheck = new ReviewDocument();
-        Comment commentCheck = new Comment();
-        List<Comment> commentListCheck = new ArrayList<Comment>();
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Forky");
+        product.setDescription("It is a fork");
+        product.setCategory("Action figure");
+
+        ReviewDocument review = new ReviewDocument();
+        review.setId(1);
+        review.setTitle("test");
+        review.setReview("Testing review");
+        review.setProduct(product);
+
+        Comment comment = new Comment();
+        comment.setId(1);
+        comment.setHeading("Test Comment");
+        comment.setComment("This is a comment body");
+
+        List<Comment> commentList = new ArrayList<Comment>();
+        commentList.add(comment);
+        review.setComments(commentList);
 
 
-        productCheck = product;
-        reviewCheck = review;
-        commentCheck = comment;
-        commentListCheck = commentListCheck;
-
-        Assert.assertNotNull(commentListCheck);
-        }
+        Assert.assertNotNull(commentList);
+    }
 
     @Test
     public void testGetComments() {
 
-//        Product product = new Product();
-//        product.setId(1);
-//        product.setName("Forky");
-//        product.setDescription("It is a fork");
-//        product.setCategory("Action figure");
-//
-//        ReviewDocument review = new ReviewDocument();
-//        review.setId(1);
-//        review.setTitle("test");
-//        review.setReview("Testing review");
-//        review.setProduct(product);
-//
-//        Comment comment = new Comment();
-//        comment.setId(1);
-//        comment.setHeading("Test Comment");
-//        comment.setComment("This is a comment body");
-//        List<Comment> commentList = new ArrayList<Comment>();
-//        commentList.add(comment);
-//        review.setComments(commentList);
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Forky");
+        product.setDescription("It is a fork");
+        product.setCategory("Action figure");
 
-        Product productCheck = new Product();
-        ReviewDocument reviewTest = new ReviewDocument();
-        Comment commentCheck = new Comment();
-        List<Comment> commentListCheck = new ArrayList<Comment>();
+        ReviewDocument review = new ReviewDocument();
+        review.setId(1);
+        review.setTitle("test");
+        review.setReview("Testing review");
+        review.setProduct(product);
 
-        reviewTest = review;
-        productCheck = product;
-        commentCheck = comment;
-        commentListCheck = commentListCheck;
+        Comment comment = new Comment();
+        comment.setId(1);
+        comment.setHeading("Test Comment");
+        comment.setComment("This is a comment body");
+        List<Comment> commentList = new ArrayList<Comment>();
+        commentList.add(comment);
+        review.setComments(commentList);
 
-        Assert.assertNotNull(reviewDocumentRepository.save(reviewTest));
+        Assert.assertNotNull(reviewDocumentRepository.save(review));
 
         //comments from the database
-//        Assert.assertNotNull(reviewDocumentRepository.findById(1).get().getComments());
-//        List<Comment> retrievedComments =
-//                reviewDocumentRepository.findById(1).get().getComments();
-//        assertEquals(1, retrievedComments.size());
-//        assertEquals(1, retrievedComments.get(0).getId().intValue());
-//        assertEquals("Test Comment", retrievedComments.get(0).getHeading());
-//        assertEquals("This is a comment body", retrievedComments.get(0).getComment());
+        Assert.assertNotNull(reviewDocumentRepository.findById(1).get().getComments());
+        List<Comment> retrievedComments =
+                reviewDocumentRepository.findById(1).get().getComments();
+        assertEquals(1, retrievedComments.size());
+        assertEquals(1, retrievedComments.get(0).getId().intValue());
+        assertEquals("Test Comment", retrievedComments.get(0).getHeading());
+        assertEquals("This is a comment body", retrievedComments.get(0).getComment());
     }
 
 
